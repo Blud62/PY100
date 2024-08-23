b781c9d1aed488ed13c0db4f757811f9ca184209
Blud62 <bludmedia@sbcglobal.net>
2024-08-23 00:28:35 -0700

    testing this out

---
+++ PY100/Functions/tranformation.py
@@ -0,0 +1,12 @@
+# Use Python's string methods on the string 'Captain Ruby' 
+# to create a string with the value 'Captain Python'.
+
+s_rube = 'Captain Ruby'
+# s_py = s_rube.split() # ['Captain', 'Ruby']
+# s_py[1] = 'Python'
+# s_py = ' '.join(s_py)
+# # s_py = s_py.pop
+# # # s_py = s_py.append('Python')
+# print(s_py)
+s_rube = s_rube.replace('Ruby', 'Python')
+print(s_rube)
